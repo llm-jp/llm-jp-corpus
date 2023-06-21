5baa7a63335da10b7e9a71b8924b5fbfd5645372
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-06-21 09:37:21 +0900

    add download script for the Pile

---
+++ scripts/data/en_pile/README.md
@@ -0,0 +1,15 @@
+# The Pile (en)
+
+Follow these instructions to create the Pile dataset.
+
+## Requirements
+
+- Python 3.9+
+
+## Download
+
+```bash
+```bash
+mkdir -p data  # or create a corresponding symlink
+./download.sh
+```
---
+++ scripts/data/en_pile/download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+DATA_DIR=data/
+BASE_URL=https://the-eye.eu/public/AI/pile/
+wget -P "${DATA_DIR} ${BASE_URL}/checksums.sha256"
+wget -P "${DATA_DIR} ${BASE_URL}/test.jsonl.zst"
+wget -P "${DATA_DIR} ${BASE_URL}/val.jsonl.zst"
+for i in $(seq -f "%02g" 0 29); do
+    wget -P "${DATA_DIR} $BASE_URL/train_${i}.jsonl.zst"
+done
