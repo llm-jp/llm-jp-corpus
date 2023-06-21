9494739b46cce3658626d3c18ba061f59f1ef8e0
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-06-21 09:44:19 +0900

    tweak

--- scripts/data/en_pile/download.sh
+++ scripts/data/en_pile/download.sh
@@ -6,5 +6,5 @@ wget -P "${DATA_DIR}" "${BASE_URL}/SHA256SUMS.txt"
 wget -P "${DATA_DIR}" "${BASE_URL}/test.jsonl.zst"
 wget -P "${DATA_DIR}" "${BASE_URL}/val.jsonl.zst"
 for i in $(seq -f "%02g" 0 29); do
-    wget -P "${DATA_DIR} $BASE_URL/train_${i}.jsonl.zst"
+    wget -P "${DATA_DIR}" "$BASE_URL/train_${i}.jsonl.zst"
 done
