8f72840e1b1c04130fa416db238f8869e971e697
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-06-21 09:54:55 +0900

    add download script for mc4

--- scripts/data/ja_cc/README.md
+++ scripts/data/ja_cc/README.md
@@ -1,1 +1,20 @@
 # CommonCrawl (ja)
+
+Follow these instructions to create the Japanese CC dataset.
+
+## Requirements
+
+- Python 3.9+
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Download
+
+```bash
+export DATA_DIR=/path/to/data/dir
+python download.py
+```
--- scripts/data/ja_cc/download.py
+++ scripts/data/ja_cc/download.py
@@ -1,1 +1,44 @@
-pass
+"""Script to download Japanese Wikipedia data.
+
+Note:
+    This script is partly borrowed from the following repository,
+    which is distributed under the Apache License 2.0.
+        https://github.com/togethercomputer/RedPajama-Data
+"""
+import argparse
+import logging
+import pathlib
+
+from datasets import load_dataset
+
+logger = logging.getLogger(__name__)
+
+LANGUAGE = "ja"
+
+
+def get_data(language: str, data_dir: pathlib.Path) -> None:
+    wiki_dataset = load_dataset("mc4", languages=[language])
+    for split, dataset in wiki_dataset.items():
+        file_path: pathlib.Path = data_dir.joinpath(f"mc4_{language}_{split}.jsonl")
+        dataset.to_json(file_path, force_ascii=False)
+        logger.info(
+            f"Finished Downloading {language}. "
+            f"There are total {len(dataset['id'])} pages."
+        )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(name)s:%(lineno)d: %(levelname)s: %(message)s",
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        help="Path to the wikipedia data directory.",
+    )
+    args = parser.parse_args()
+
+    get_data(LANGUAGE, data_dir=pathlib.Path(args.data_dir))
