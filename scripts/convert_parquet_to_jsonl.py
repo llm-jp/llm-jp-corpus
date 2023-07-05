057fb858605359e3fab9b211b5ac4fcd97f304c2
nobu-g <ueda-n127@hotmail.co.jp>
2023-07-05 18:14:50 +0900

    fix convert_parquet_to_jsonl.py for multiprocessing

--- scripts/convert_parquet_to_jsonl.py
+++ scripts/convert_parquet_to_jsonl.py
@@ -1,11 +1,26 @@
 import logging
 import pathlib
 from argparse import ArgumentParser
+from concurrent.futures import ProcessPoolExecutor
 
-from datasets import Dataset
+from datasets import Dataset, disable_caching
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
+disable_caching()
+
+
+def process_file(
+    input_file: pathlib.Path, output_dir: pathlib.Path, overwrite: bool
+) -> None:
+    logger.info(f"Writing the dataset to {output_dir} in JSONL format.")
+    output_file: pathlib.Path = output_dir.joinpath(f"{input_file.stem}.jsonl")
+    if output_file.exists() and not overwrite:
+        logger.error(f"{output_file} already exists. Specify --overwrite to continue.")
+        return
+    dataset: Dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
+    dataset.to_json(output_file, force_ascii=False)
+    logger.info(f"Finished exporting to {output_file}.")
 
 
 def main() -> None:
@@ -25,26 +40,21 @@ def main() -> None:
         action="store_true",
         help="Whether to overwrite the output directory.",
     )
+    parser.add_argument(
+        "--num_proc",
+        type=int,
+        default=1,
+        help="Number of processes for parallel execution.",
+    )
     args = parser.parse_args()
 
     data_dir: pathlib.Path = pathlib.Path(args.data_dir)
     output_dir: pathlib.Path = pathlib.Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    logger.info("Loading the dataset")
-    for input_file in tqdm(data_dir.glob("*.parquet")):
-        logger.info(f"Loading {input_file}.")
-        dataset: Dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
-
-        logger.info(f"Writing the dataset to {output_dir} in JSONL format.")
-        output_file: pathlib.Path = output_dir.joinpath(f"{input_file.stem}.jsonl")
-        if output_file.exists() and not args.overwrite:
-            logger.error(
-                f"{output_file} already exists. Specify --overwrite to continue."
-            )
-            exit(1)
-        dataset.to_json(output_file, force_ascii=False)
-        logger.info(f"Finished exporting to {output_file}.")
+    with ProcessPoolExecutor(max_workers=args.num_proc) as executor:
+        for input_file in tqdm(data_dir.glob("*.parquet")):
+            executor.submit(process_file, input_file, output_dir, args.overwrite)
 
 
 if __name__ == "__main__":
