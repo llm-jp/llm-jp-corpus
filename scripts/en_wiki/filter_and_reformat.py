cdc40c16a5c6701c5e3760c2f061ebc150d1c5e6
nobu-g <ueda-n127@hotmail.co.jp>
2023-07-05 18:17:24 +0900

    fix a bug

--- scripts/en_wiki/filter_and_reformat.py
+++ scripts/en_wiki/filter_and_reformat.py
@@ -9,12 +9,14 @@ LANGUAGE = "en"
 DUMP_DATE = "20230320"
 
 
-def filter_and_reformat(examples) -> dict[str, Any]:
+def filter_and_reformat(example) -> dict[str, Any]:
     return {
-        "text": examples["text"],
-        "title": examples["title"],
-        "url": examples["url"],
-        "language": [LANGUAGE] * len(examples["text"]),
-        "timestamp": [DUMP_DATE] * len(examples["text"]),
-        "source": [DATASET_NAME] * len(examples["text"]),
+        "text": example["text"],
+        "meta": {
+            "title": example["title"],
+            "url": example["url"],
+            "language": LANGUAGE,
+            "timestamp": DUMP_DATE,
+            "source": DATASET_NAME,
+        },
     }
