13493dc2080d9a1c9abe6f32b115b5b70cb6d6d7
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-08-02 10:49:48 +0900

    refactoring

--- scripts/filter_and_reformat_data.py
+++ scripts/filter_and_reformat_data.py
@@ -1,19 +1,15 @@
-import json
 import logging
 import pathlib
 import time
+import typing
 from argparse import ArgumentParser
 from typing import Any, Callable
+from urllib.parse import urlparse
 
+import regex
 import tqdm
-from code_stack.filter_and_reformat import filter_and_reformat as filter_code_stack
 from datasets import Dataset, IterableDatasetDict, disable_caching, load_dataset
 from datasets.splits import Split
-from en_pile.filter_and_reformat import filter_and_reformat as filter_en_pile
-from en_wiki.filter_and_reformat import filter_and_reformat as filter_en_wiki
-from ja_cc.filter_and_reformat import filter_and_reformat as filter_ja_cc
-from ja_wiki.filter_and_reformat import filter_and_reformat as filter_ja_wiki
-from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 disable_caching()
@@ -34,6 +30,72 @@ def get_data_files(search_dir: pathlib.Path, ext: str) -> dict[Split, pathlib.Pa
     if len(test_files) == 1:
         data_files[Split.TEST] = test_files[0]
     return data_files
+
+
+def reformat_builder(text_field: str) -> Callable[..., dict[str, Any]]:
+    def reformat(example: dict[str, Any]) -> dict[str, Any]:
+        return {
+            "text": example[text_field],
+            "meta": {
+                **{k: v for k, v in example.items() if k != text_field},
+            },
+        }
+
+    return reformat
+
+
+def has_valid_domain(example: dict[str, Any]) -> bool:
+    if example["meta"]["url"].startswith("https://ja.wikipedia.org/"):
+        return False
+    domain: typing.Optional[str] = urlparse(example["meta"]["url"]).hostname
+    assert domain is not None
+    tld: str = domain.split(".")[-1]
+    return tld in {
+        "jp",
+        "com",
+        "net",
+        "org",
+        "work",
+        "info",
+        "xyz",
+        "biz",
+        "work",
+        "me",
+        "tv",
+        "site",
+        "tokyo",
+        "cc",
+    }
+
+
+def has_empty_text(example: dict[str, Any]) -> bool:
+    return example["text"].strip() != ""
+
+
+def remove_non_japanese_text(example: dict[str, Any]) -> dict[str, Any]:
+    ja_pat = regex.compile(r"[\p{Script=Hiragana}\p{Script=Katakana}ー]+")
+    script_pat = regex.compile(
+        r"[\u0000-\u007F\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]{100,}"
+    )
+    url_pat = regex.compile(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+")
+
+    def regex_filter(sentence: str, pat) -> str:
+        valid: str = ""
+        index: int = 0
+        for m in pat.finditer(sentence):
+            valid += sentence[index : m.start()]
+            index = m.end()
+        valid += sentence[index:]
+        return valid
+
+    valid: str = ""
+    for sentence in example["text"].split("\n"):
+        if ja_pat.search(sentence):
+            sentence = regex_filter(sentence, url_pat)
+            sentence = regex_filter(sentence, script_pat)
+            valid += sentence
+    example["text"] = valid
+    return example
 
 
 def main() -> None:
@@ -74,34 +136,37 @@ def main() -> None:
         streaming=True,
     )
 
-    filter_fn: Callable[..., dict[str, Any]]
+    map_fns: list[Callable[..., dict[str, Any]]] = []
+    filter_fns: list[Callable[..., bool]] = []
     if args.DATASET_NAME == "ja_wiki":
-        filter_fn = filter_ja_wiki
+        map_fns.append(reformat_builder("text"))
+        filter_fns.append(has_empty_text)
     elif args.DATASET_NAME == "en_wiki":
-        filter_fn = filter_en_wiki
+        map_fns.append(reformat_builder("text"))
+        filter_fns.append(has_empty_text)
     elif args.DATASET_NAME == "ja_cc":
-        filter_fn = filter_ja_cc
+        map_fns.append(reformat_builder("text"))
+        map_fns.append(remove_non_japanese_text)
+        filter_fns.append(has_valid_domain)
+        filter_fns.append(has_empty_text)
     elif args.DATASET_NAME == "en_pile":
-        filter_fn = filter_en_pile
+        map_fns.append(reformat_builder("text"))
+        filter_fns.append(has_empty_text)
     elif args.DATASET_NAME == "code_stack":
-        filter_fn = filter_code_stack
+        map_fns.append(reformat_builder("content"))
+        filter_fns.append(has_empty_text)
     else:
         raise ValueError(f"Unknown dataset name: {args.DATASET_NAME}.")
 
-    dataset = dataset.map(
-        filter_fn,
-        remove_columns=list(
-            set(list(dataset["train"].take(1))[0].keys()) - {"text", "meta"}
-        ),
-        batched=False,
-    ).filter(
-        lambda example: example["text"] != "",
-    )
+    for map_fn in map_fns:
+        dataset = dataset.map(map_fn, batched=False)
+    for filter_fn in filter_fns:
+        dataset = dataset.filter(filter_fn)
 
     logger.info(f"Writing the reformatted data to {output_dir}.")
     for split, ds in dataset.items():
         chunk_index = 0
-        for batch in tqdm(ds.iter(batch_size=CHUNK_SIZE)):
+        for batch in tqdm.tqdm(ds.iter(batch_size=CHUNK_SIZE)):
             output_file: pathlib.Path = output_dir.joinpath(
                 f"{split}_{chunk_index}.parquet"
             )
