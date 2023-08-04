c841664a8c7a2e708dd3f0969d58cbbd8223440d
nobu-g <ueda-n127@hotmail.co.jp>
2023-08-04 19:11:53 +0900

    fix tokenize_data.py for more flexible input

--- scripts/tokenize_data.py
+++ scripts/tokenize_data.py
@@ -15,7 +15,7 @@ disable_caching()
 sentence_piece_processor: spm.SentencePieceProcessor
 
 
-def tokenize_function(examples: dict[str, Any]) -> dict[str, Any]:
+def tokenize_examples(examples: dict[str, Any]) -> dict[str, Any]:
     token_ids: list[list[int]] = [
         sentence_piece_processor.encode_as_ids(text) for text in examples["text"]
     ]
@@ -26,12 +26,33 @@ def tokenize_function(examples: dict[str, Any]) -> dict[str, Any]:
     }
 
 
+def tokenize_file(
+    input_file: pathlib.Path, output_file: pathlib.Path, num_proc: int
+) -> None:
+    logger.info(f"Loading {input_file}.")
+    dataset: Dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
+    logger.info("Tokenizing the dataset.")
+    dataset = dataset.map(
+        tokenize_examples,
+        batched=True,
+        batch_size=128,
+        keep_in_memory=True,
+        num_proc=num_proc,
+    )
+    logger.info("Finished tokenizing the dataset.")
+
+    logger.info(f"Writing the tokenized data to {output_file}.")
+    dataset.to_parquet(output_file)
+    logger.info(f"Finished writing the tokenized to {output_file}.")
+
+
 def main() -> None:
     parser = ArgumentParser()
     parser.add_argument(
-        "--data_dir",
+        "--input_path",
         type=str,
-        help="Path to the wikipedia data directory.",
+        nargs="+",
+        help="Path(s) to the input data directory or file.",
     )
     parser.add_argument(
         "--output_dir",
@@ -56,7 +77,6 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    data_dir: pathlib.Path = pathlib.Path(args.data_dir)
     output_dir: pathlib.Path = pathlib.Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -67,29 +87,23 @@ def main() -> None:
     sentence_piece_processor = spm.SentencePieceProcessor(args.sentencepiece_model)
 
     logger.info("Loading the dataset")
-    for input_file in tqdm(data_dir.glob("*.parquet")):
-        output_file: pathlib.Path = output_dir.joinpath(input_file.name)
-        if output_file.exists() and not args.overwrite:
-            logger.error(
-                f"{output_file} already exists. Specify --overwrite to overwrite."
-            )
+    for path_str in tqdm(args.input_path):
+        path = pathlib.Path(path_str)
+        if path.exists() is False:
+            logger.warning(f"{path} not found and skipped")
             continue
-
-        logger.info(f"Loading {input_file}.")
-        dataset: Dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
-        logger.info("Tokenizing the dataset.")
-        dataset = dataset.map(
-            tokenize_function,
-            batched=True,
-            batch_size=128,
-            keep_in_memory=True,
-            num_proc=os.cpu_count() if args.num_proc == -1 else args.num_proc,
-        )
-        logger.info("Finished tokenizing the dataset.")
-
-        logger.info(f"Writing the tokenized data to {output_dir}.")
-        dataset.to_parquet(output_file)
-        logger.info(f"Finished writing the tokenized to {output_file}.")
+        for input_file in tqdm(path.glob("*.parquet") if path.is_dir() else [path]):
+            output_file: pathlib.Path = output_dir.joinpath(input_file.name)
+            if output_file.exists() and not args.overwrite:
+                logger.error(
+                    f"{output_file} already exists. Specify --overwrite to overwrite."
+                )
+                continue
+            tokenize_file(
+                input_file,
+                output_file,
+                num_proc=os.cpu_count() if args.num_proc == -1 else args.num_proc,
+            )
 
     end_time = time.time()
     logger.info(
