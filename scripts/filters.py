cd2032f31fb37eb6ed079c9c613fa1449e9c7773
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-08-09 16:56:51 +0900

    tweak

--- scripts/filters.py
+++ scripts/filters.py
@@ -181,6 +181,7 @@ def remove_wikipedia_footnote(example: dict[str, Any]) -> dict[str, Any]:
         "脚注",
         "関連項目",
         "日本国内の関連項目",
+        "出典",
         "出典・脚注",
         "参照",
         "外部リンク",
