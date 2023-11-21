e2add34bf5da7a51be7a4151c67ba27d84955a8c
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-11-21 19:18:37 +0900

    add filtering script for english data in v2

--- scripts/filter_data.py
+++ scripts/filter_data.py
@@ -37,21 +37,18 @@ from filters import (
 logger = logging.getLogger(__name__)
 disable_caching()
 
-CHUNK_SIZE = 100_000
+CHUNK_SIZE = 10_000_000
 
-
-def get_data_files(search_dir: pathlib.Path, ext: str) -> dict[Split, pathlib.Path]:
-    train_files = list(search_dir.glob(f"*train*.{ext}"))
-    valid_files = list(search_dir.glob(f"*valid*.{ext}"))
-    test_files = list(search_dir.glob(f"*test*.{ext}"))
-    assert len(train_files) == 1, f"Found {len(train_files)} train files."
-    assert len(valid_files) <= 1, f"Found {len(valid_files)} valid files."
-    assert len(test_files) <= 1, f"Found {len(test_files)} test files."
-    data_files = {Split.TRAIN: train_files[0]}
-    if len(valid_files) == 1:
-        data_files[Split.VALIDATION] = valid_files[0]
-    if len(test_files) == 1:
-        data_files[Split.TEST] = test_files[0]
+def get_data_files(search_dir: pathlib.Path, ext: str) -> dict[Split, str]:
+    data_files = {}
+    if len(list(search_dir.glob(f"*train*.{ext}"))):
+        data_files[Split.TRAIN] = str(search_dir.joinpath(f"*train*.{ext}"))
+    if len(list(search_dir.glob(f"*valid*.{ext}"))):
+        data_files[Split.VALIDATION] = str(search_dir.joinpath(f"*valid*.{ext}"))
+    if len(list(search_dir.glob(f"*test*.{ext}"))):
+        data_files[Split.TEST] = str(search_dir.joinpath(f"*test*.{ext}"))
+    if len(data_files) == 0:
+        raise ValueError(f"No data files found in {search_dir}.")
     return data_files
 
 
@@ -98,6 +95,12 @@ def reformat_and_filter_dataset(
         filter_fns.append(has_valid_avg_line_length())
         filter_fns.append(has_valid_alphanum_fraction())
         filter_fns.append(is_not_empty())
+    elif dataset_name == "en_slimpajama":
+        reformat_fn = reformat_data("text")
+        filter_fns.append(is_not_empty())
+    elif dataset_name == "en_refinedweb":
+        reformat_fn = reformat_data("content")
+        filter_fns.append(is_not_empty())
     else:
         raise ValueError(f"Unknown dataset name: {dataset_name}.")
 
@@ -122,7 +125,17 @@ def main() -> None:
     parser.add_argument(
         "DATASET_NAME",
         type=str,
-        choices=["ja_wiki", "en_wiki", "ja_cc", "en_pile", "code_stack"],
+        choices=[
+            # v1
+            "ja_wiki",
+            "en_wiki",
+            "ja_cc",
+            "en_pile",
+            "code_stack",
+            # v2
+            "en_slimpajama",
+            "en_refinedweb",
+        ],
         help="Dataset name",
     )
     parser.add_argument(
@@ -145,6 +158,20 @@ def main() -> None:
         action="store_true",
         help="Whether to overwrite the output directory.",
     )
+    parser.add_argument(
+        "--input_format",
+        type=str,
+        default="parquet",
+        choices=["parquet", "json"],
+        help="Whether to load the dataset from parquet or json.",
+    )
+    parser.add_argument(
+        "--output_format",
+        type=str,
+        default="parquet",
+        choices=["parquet", "json"],
+        help="Whether to save the dataset to parquet or json.",
+    )
     args = parser.parse_args()
 
     input_dir: pathlib.Path = pathlib.Path(args.input_dir)
@@ -154,22 +181,39 @@ def main() -> None:
     start_time = time.time()
 
     logger.info("Loading the dataset")
-    dataset: DatasetDict = load_dataset(
-        "json",
-        data_files={k: str(v) for k, v in get_data_files(input_dir, "jsonl").items()},
-        streaming=True,
-    )
+    if args.input_format == "json":
+        dataset: DatasetDict = load_dataset(
+            "json",
+            data_files=get_data_files(input_dir, "jsonl"),
+            streaming=True,
+        )
+    elif args.input_format == "parquet":
+        dataset: DatasetDict = load_dataset(
+            "parquet",
+            data_files=get_data_files(input_dir, "parquet"),
+            streaming=True,
+        )
+    else:
+        raise ValueError(f"Unknown input format: {args.input_format}.")
 
     dataset = reformat_and_filter_dataset(
         dataset, args.DATASET_NAME, strict=args.strict
     )
+
+    ext: str
+    if args.output_format == "json":
+        ext = "jsonl"
+    elif args.output_format == "parquet":
+        ext = "parquet"
+    else:
+        raise ValueError(f"Unknown output format: {args.output_format}.")
 
     logger.info(f"Writing the reformatted data to {output_dir}.")
     for split, ds in dataset.items():
         chunk_index = 0
         for batch in tqdm.tqdm(ds.iter(batch_size=CHUNK_SIZE)):
             output_file: pathlib.Path = output_dir.joinpath(
-                f"{split}_{chunk_index}.parquet"
+                f"{split}_{chunk_index}.{ext}"
             )
             if output_file.exists() and not args.overwrite:
                 logger.error(
@@ -178,7 +222,12 @@ def main() -> None:
                 chunk_index += 1
                 continue
 
-            Dataset.from_dict(batch).to_parquet(output_file)
+            if args.output_format == "json":
+                Dataset.from_dict(batch).to_json(output_file, force_ascii=False)
+            elif args.output_format == "parquet":
+                Dataset.from_dict(batch).to_parquet(output_file)
+            else:
+                raise ValueError(f"Unknown output format: {args.output_format}.")
             chunk_index += 1
 
     end_time = time.time()
